937ec3510df15aecf7491adcfb5825823d57b517
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 19:43:15 +0800

    feat: Init commit
    init node-rollup-plugin-node-polyfills
    
    Log:

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+
+export default {
+	input: 'dist-transpiled/index.js',
+	external: [
+		'path',
+		'crypto',
+		'rollup-plugin-inject'
+	],
+	output: [
+		{
+			format: 'es',
+			file: 'dist/index.mjs',
+			preferConst: true,
+		},
+		{
+			format: 'cjs',
+			file: 'dist/index.js',
+			preferConst: true,
+		}
+	]
+};
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+// @ts-ignore
+import inject from 'rollup-plugin-inject';
+import { builtinsResolver, NodePolyfillsOptions } from './modules';
+import { dirname, relative } from 'path';
+import { randomBytes } from 'crypto';
+
+export default function (opts: NodePolyfillsOptions = {}) {
+  const injectPlugin = inject({
+    include: opts.include === undefined ? 'node_modules/**/*.js' : undefined,
+    exclude: opts.exclude,
+    sourceMap: opts.sourceMap,
+    modules: {
+      'process': 'process',
+      'Buffer': ['buffer', 'Buffer'],
+      'global': GLOBAL_PATH,
+      '__filename': FILENAME_PATH,
+      '__dirname': DIRNAME_PATH,
+    }
+  });
+  const basedir = opts.baseDir || '/';
+  const dirs = new Map<string, string>();
+  const resolver = builtinsResolver(opts);
+  return {
+    name: 'node-polyfills',
+    resolveId(importee: string, importer: string) {
+      if (importee === DIRNAME_PATH) {
+        const id = getRandomId();
+        dirs.set(id, dirname('/' + relative(basedir, importer)));
+        return {id, moduleSideEffects: false};
+      }
+      if (importee === FILENAME_PATH) {
+        const id = getRandomId();
+        dirs.set(id, dirname('/' + relative(basedir, importer)));
+        return {id, moduleSideEffects: false};
+      }
+      return resolver(importee);
+    },
+    load(id: string) {
+      if (dirs.has(id)) {
+        return `export default '${dirs.get(id)}'`;
+      }
+    },
+    transform(code: string, id: string)  {
+      return injectPlugin.transform.call(this, code, id);
+    }
+  };
+}
+
+function getRandomId() {
+  return randomBytes(15).toString('hex');
+}
+
+const GLOBAL_PATH = require.resolve('../polyfills/global.js');
+const DIRNAME_PATH = '\0node-polyfills:dirname';
+const FILENAME_PATH = '\0node-polyfills:filename';
